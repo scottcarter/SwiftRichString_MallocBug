73a816b1b13b387e7fab1be07d1130c5711c2770
Scott Carter <scarter28m-github@yahoo.com>
2017-12-19 08:04:58 -0500

    Initial Commit

---
+++ SwiftRichString_MallocBug/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftRichString_MallocBug
+//
+//  Created by Scott Carter on 12/19/17.
+//  Copyright © 2017 Scott Carter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftRichString_MallocBug/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftRichString_MallocBug
+//
+//  Created by Scott Carter on 12/19/17.
+//  Copyright © 2017 Scott Carter. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
